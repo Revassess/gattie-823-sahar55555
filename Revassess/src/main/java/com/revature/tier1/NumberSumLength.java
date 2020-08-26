383a474baac36049b175d1a683fdb8a2bd225578
sahar55555 <sahar1elkady@hotmail.com>
2020-08-26 13:36:28 -0600

    first tier

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -1,8 +1,25 @@
 package com.revature.tier1;
 
+import java.util.Scanner;
+
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		 long  n, sum = 0;
+		 int pwr,cnt=0;
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter the number:");
+	        num = sc.nextInt();
+	        while(num >= 0)
+	        {
+	        	cnt++;
+	        	num/=10;
+	        
+	            n=num%10;
+	            sum+=Math.pow(n, cnt);
+	        }
+	        if(sum==num)
+	        	return true;
 		return false;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,13 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int len=arr.length;
+		int sum=0;
+		for(int i=0;i<len;i++)
+		{
+			sum+=arr[i];
+		}
+		
+		return sum;
 	}
 }
